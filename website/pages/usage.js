b54a2bdf8c9efbdc982273a69dfdd353186bbb85
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-06-12 23:03:39 -0400

    refactored to make usage page static

--- website/pages/usage.js
+++ website/pages/usage.js
@@ -4,10 +4,11 @@ import LineGraph from "../components/graphs/LineGraph";
 
 import db from "../util/db";
 
-import { Grid } from "@chakra-ui/react";
+import { Grid, Skeleton } from "@chakra-ui/react";
 import { DateTime } from "luxon";
+import { useState, useEffect } from "react";
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
     // Get reference and snapshot to weekly generation data
     const wegRef = db.collection("data").doc("weekly").collection("generation").orderBy("date");
     const wegQSnapshot = await wegRef.get();
@@ -37,41 +38,60 @@ export async function getServerSideProps(context) {
         // Push to global data
         generationData.labels.push(label);
         generationData.datasets[0].data.push(generated);
-    }); 
-
-
-    // Get notifications from server
-    const userDataRef = db.collection("users");
-    const userDataSnapshot = await userDataRef.get();
-
-    // Data format for other components
-    let userData = [];
-
-    // Get data from snapshot
-    userDataSnapshot.forEach((doc) => {
-        userData.push({
-            id: doc.id,
-            ...doc.data()
-        }); // Layout in database matches layout required
     });
 
     return {
         props: {
-            generationData,
-            userData
+            generationData
         }
     }
 }
 
-export default function Usage({ generationData, userData }) {
-    userData.sort((a, b) => b.weeklyUsage - a.weeklyUsage); // Sort by weekly usage (descending)
+export default function Usage({ generationData }) {
+    const [userData, setUserData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(async () => {
+        // Set to be loading
+        setIsLoading(true);
+
+        // Get notifications from server
+        const userDataRef = db.collection("users");
+        const userDataSnapshot = await userDataRef.get();
+
+        // Data format for other components
+        let userDataTmp = [];
+
+        // Get data from snapshot
+        userDataSnapshot.forEach((doc) => {
+            userDataTmp.push({
+                id: doc.id,
+                ...doc.data()
+            }); // Layout in database matches layout required
+        });
+
+        userDataTmp.sort((a, b) => b.weeklyUsage - a.weeklyUsage); // Sort by weekly usage (descending)
+
+        setUserData(userDataTmp);
+
+        setIsLoading(false);
+
+        return () => {
+            setIsLoading(false);
+        }
+    }, []);
 
     return (
         <Layout name="Usage">
-            <UsageOrderedList data={userData} />
-            <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-                <LineGraph title="Weekly Energy Usage" data={generationData} />
-            </Grid>
+            <Skeleton isLoaded={!isLoading}>
+                <UsageOrderedList data={userData} />
+            </Skeleton>
+
+            <Skeleton isLoaded={!isLoading}>
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <LineGraph title="Weekly Energy Usage" data={generationData} />
+                </Grid>
+            </Skeleton>
         </Layout>
     )
 }
